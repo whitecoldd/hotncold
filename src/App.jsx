0de91679fc8a8418c1c7a014127c41ecbc29272d
whitecoldd <buyukliwc@gmail.com>
2023-05-16 16:57:40 +0200

    head added

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,28 @@ import Categories from "./components/Categories";
 import Pages from "./pages/Pages";
 import Search from "./components/Search";
 import { TbSoup, TbSalad } from "react-icons/tb";
+import { Helmet } from "react-helmet";
 function App() {
   return (
-    <div className="mx-36">
-      <BrowserRouter>
-        <Link to={"/"} className="flex items-center text-2xl mt-10 mb-5">
-          <TbSoup  />
-          <h1 className="font-ananda mr-1">hotncold</h1>
-          <TbSalad />
-        </Link>
-        <Search />
-        <Categories />
-        <Pages />
-      </BrowserRouter>
-    </div>
+    <>
+      <Helmet>
+        <title>hotncold</title>
+        <meta name="hotncold" content="hotncold" />
+        <link rel="icon" href="/hotncold.ico" />
+      </Helmet>
+      <div className="mx-36">
+        <BrowserRouter>
+          <Link to={"/"} className="flex items-center text-2xl mt-10 mb-5">
+            <TbSoup />
+            <h1 className="font-ananda mr-1">hotncold</h1>
+            <TbSalad />
+          </Link>
+          <Search />
+          <Categories />
+          <Pages />
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
