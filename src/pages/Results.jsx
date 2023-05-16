3dbaa1d9c7a31111b81cd314a6dd1abd315ed146
whitecoldd <buyukliwc@gmail.com>
2023-05-17 01:49:55 +0200

    added empty query response

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -17,7 +17,19 @@ const Results = () => {
     };
     getResults(params.dish);
   }, [params.dish]);
-
+  if (recipees.length <= 0) {
+    return (
+      <div className="text-center my-10">
+        <h1 className="text-2xl">No recipes were found for this query.</h1>
+        <Link
+          to={"/"}
+          className="text-3xl text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-cyan-600 "
+        >
+          Go to home page
+        </Link>
+      </div>
+    );
+  }
   return (
     <div className="griddiv">
       {recipees.map((re, i) => (
